74e7878164380cfd4ea59a5d10a811b13802b074
ohletamyleta <ohletamyleta@gmail.com>
2021-02-03 16:06:07 -0500

    seeded database for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController 
 
+  get '/' do
+    erb:home, :register => :'shows/list_shows'
+  end
+
+  get '/register' do
+
+  end
+
+  post '/register' do
+
+  end
+
+  
 
 
 
