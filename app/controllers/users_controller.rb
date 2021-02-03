2d5f59310d28d75c9d2bcb9d8f2bf1b777b471af
ohletamyleta <ohletamyleta@gmail.com>
2021-02-03 16:19:16 -0500

    built register get/post routes in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,43 @@ class UsersController < ApplicationController
   end
 
   get '/register' do
-
+    if Helpers.is_logged_in?(session)
+      redirect '/list_shows'
+    else
+    erb :'/users/register'
+    end
   end
 
   post '/register' do
-
+    if Helpers.is_logged_in?(session)
+      redirect to '/list_shows'
+    end
+      if !(params.has_value?(""))
+        @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect "/list_shows"
+    else
+      redirect to "/register"
+    end
+  erb :'shows/list_shows'
   end
 
-  
+  # get '/login' do
+
+  # end
+
+  # post '/login' do
+
+  # end
+
+  # get '/users/:slug' do
+
+  # end
+
+  # get '/logout' do
+
+  # end
+   
 
 
 
