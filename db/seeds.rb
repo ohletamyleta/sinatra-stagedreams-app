c129dc7637722ddcbdc668880e20051db93bef83
ohletamyleta <ohletamyleta@gmail.com>
2021-02-08 20:33:07 -0500

    rollback and re-migrate to clear test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@ gina = User.create(username: "Gina", email: "gina@gina.com", password: "password
 Show.create(title: "Godspell", author: "Some Guy", role: "Judas", style: "musical", year_done: "2021", user_id: steve.id)
 Show.create(title: "Avenue Q", author: "Some Dude", role: "Lucy the Slut", style: "musical", year_done: "2015", user_id: gina.id)
 
-steve.shows.create(title: "Avenue Q", author: "Some Dude", role: "Gary Coleman", style: "musical", year_done: "2019")
